fec714204cf6c1a7700bc2c8b35209cb7c683262
Brainor <brainor@qq.com>
2024-01-10 00:49:52 +0800

    feat: 使用tqdm

--- down.py
+++ down.py
@@ -4,11 +4,12 @@ import re
 import time
 import urllib.parse as parse
 from configparser import ConfigParser
+from multiprocessing.connection import Connection
 from pathlib import Path
 
 import fire
 import requests
-import tqdm
+from tqdm import tqdm
 
 s = requests.Session()
 config = ConfigParser()
@@ -26,64 +27,66 @@ def main(record: str):
     start_loc = file_loc.stat().st_size if file_loc.exists() else 0
     start = time.time()
     print(f"下载: {file_loc.name}")
-    while "speed_dict" not in locals() or speed_dict["stop"]:
-        speed_dict = mp.Manager().dict(
-            {
-                "size": [0] * 10,
-                "time": [time.time()] * 10,
-                "total_length": -1,
-                "current_length": -2,
-                "stop": False,  # 由进度条控制是否重置连接. TODO 0正常, 1速度太慢未考虑proxy, 2速度太慢已考虑proxy
-                "max_speed": 0,  # 0表示无法正常连接
-            }
-        )
-        p_progress_bar = mp.Process(target=progress_bar, args=(speed_dict,), daemon=True)
-        p_progress_bar.start()
+    restart = True  # 是否进入循环
+    pipe_recv, pipe_send = mp.Pipe(duplex=False)
+    while restart:
+        pbar = None
+        max_speed = 0  # 0表示无法正常连接
+        total_length = -1
+        current_length = -2
+        slow_flag = 0
 
-        p_download_file = mp.Process(target=emby_download, args=(url, file_loc, speed_dict), daemon=True)
+        p_download_file = mp.Process(target=emby_download, args=(url, file_loc, pipe_send), daemon=True)
         p_download_file.start()
         while True:
-            if speed_dict["stop"]:  # 速度太慢
+            if pipe_recv.poll():
+                if pbar is None:
+                    total_length, current_length = pipe_recv.recv()
+                    pbar = tqdm(
+                        total=total_length,
+                        initial=current_length,
+                        mininterval=1,
+                        unit_scale=True,
+                        unit_divisor=1024,
+                        unit="B",
+                        bar_format = "|{bar:50}| {rate_fmt} {n_fmt}/{total_fmt} 剩余: {remaining}",
+                    )
+                else:
+                    n = pipe_recv.recv()
+                    pbar.update(n)
+                    current_length += n
+            if pbar is None or (rate := pbar.format_dict["rate"]) is None:
+                rate = 0
+            max_speed = max(max_speed, rate)
+            if rate <= max_speed / 2 and rate < 1024 * 1024:
+                slow_flag += 1
+            else:
+                slow_flag = 0
+            if slow_flag >= 30:
+                pbar.close()
+                print("\n速度太慢, 5秒后重新连接")
                 p_download_file.terminate()
                 p_download_file.join()
-                if p_progress_bar.is_alive():
-                    p_progress_bar.terminate()
-                    p_progress_bar.join()
-                print("\n速度太慢, 5秒后重新连接")
                 time.sleep(5)
                 break
             elif not p_download_file.is_alive():
-                if speed_dict["current_length"] == speed_dict["total_length"] and speed_dict["total_length"] > 0:  # 下载完成
-                    p_progress_bar.terminate()
-                    p_progress_bar.join()
+                if current_length == total_length and total_length > 0:  # 下载完成
+                    pbar.close()
+                    restart = False
                     break
                 else:  # 下载报错, p_download_file停止
-                    if p_progress_bar.is_alive():
-                        p_progress_bar.terminate()
-                        p_progress_bar.join()
+                    pbar.close()
                     print("\n下载失败, 5秒后重新连接")
-                    speed_dict["stop"] = True
                     time.sleep(5)
                     break
             time.sleep(1)
-
-        # method_iter_conent(file_loc, response, speed_dict) # 使用iter_content, 无法解决在某时刻没有速度的情况, 且最大速度为1M/s
-        # method_shutil(file_loc, response, speed_dict)  # 使用 shutil.copyfileobj, 最快能达8M/s
-        # if mp.active_children():
-        #     p_progress_bar.terminate()
-        #     p_progress_bar.close()
-        # else:
-        #     print('已经关闭')
-        # if speed_dict['stop']:
-        #     print('速度太慢, 5秒后重新连接')
-        #     time.sleep(5)
+        pbar = None
 
     end = time.time()
-    print()
-    print(f'耗时: {(end - start):.2f}s, 平均速度: {sizeof_fmt((speed_dict["total_length"]-start_loc)/(end - start))}/s\n')
+    print(f"耗时: {(end - start):.2f}s, 平均速度: {tqdm.format_sizeof((total_length-start_loc)/(end - start), suffix="B", divisor=1024)}/s\n")
 
 
-def emby_download(url: str, file_loc: Path, speed_dict):
+def emby_download(url: str, file_loc: Path, pipe_send: Connection):
     o = parse.urlsplit(url)
     if file_loc.exists():
         start_loc = file_loc.stat().st_size
@@ -95,65 +98,35 @@ def emby_download(url: str, file_loc: Path, speed_dict):
     try:
         with s.get(url, headers=header, stream=True) as response:
             response.raise_for_status()
-            total_length = response.headers.get("content-length")
-            if total_length is None:  # no content length header
+            total_length_raw = response.headers.get("content-length")
+            if total_length_raw is None:  # no content length header
                 return response.content
 
-            speed_dict["total_length"] = int(response.headers["Content-Range"].split("/")[1])
-            speed_dict["current_length"] = start_loc
-            # method_iter_conent(file_loc, response, speed_dict) # 使用iter_content, 无法解决在某时刻没有速度的情况, 且最大速度为1M/s
-            method_shutil(file_loc, response, speed_dict)  # 使用 shutil.copyfileobj, 最快能达8M/s
+            pipe_send.send((int(response.headers["Content-Range"].split("/")[1]), start_loc))
+            # method_iter_conent(file_loc, response, current_length) # 使用iter_content, 无法解决在某时刻没有速度的情况, 且最大速度为1M/s
+            method_shutil(file_loc, response, pipe_send)  # 使用 shutil.copyfileobj, 最快能达8M/s
     except Exception as e:
         print(response.headers)
         raise e
 
 
-def method_shutil(file_loc: Path, response: requests.Response, speed_dict):
-    speed_index = 0
+def method_shutil(file_loc: Path, response: requests.Response, pipe_send: Connection):
     with open(file_loc, "ab") as f:  # https://stackoverflow.com/a/29967714/5340217
         length = 16 * 1024 * 1024
         with memoryview(bytearray(length)) as mv:
-            while not speed_dict["stop"]:
+            while True:
                 n = response.raw.readinto(mv)
                 if not n:
+                    print("\nnot n\n")
                     break
                 elif n < length:
+                    pipe_send.send(n)
                     with mv[:n] as smv:
                         f.write(smv)
-
-                        speed_index = update_speed(speed_dict, n, speed_index)
                     break
                 else:
+                    pipe_send.send(n)
                     f.write(mv)
-
-                    speed_index = update_speed(speed_dict, n, speed_index)
-
-
-def method_iter_conent(file_loc, response, speed_dict):
-    with open(file_loc, "ab") as f:
-        for data in response.iter_content(chunk_size=10 * 1024 * 1024):
-            f.write(data)
-
-            update_speed(speed_dict, len(data))
-
-
-def update_speed(speed_dict, n, speed_index):
-    speed_size = speed_dict["size"]
-    speed_size[speed_index] = n
-    speed_time = speed_dict["time"]
-    speed_time[speed_index] = time.time()
-    speed_dict["size"] = speed_size  # https://stackoverflow.com/a/37510417/5340217
-    speed_dict["time"] = speed_time
-    speed_dict["current_length"] += n
-    return (speed_index + 1) % 10
-
-
-def sizeof_fmt(num, suffix="B"):  # https://stackoverflow.com/a/1094933/5340217
-    for unit in ["", "k", "M", "G", "T", "P", "E", "Z"]:
-        if abs(num) < 1024.0:
-            return f"{num:3.2f}{unit}{suffix}"
-        num /= 1024.0
-    return f"{num:.2f}Y{suffix}"
 
 
 def get_filename(url: str, folder_loc: str):
@@ -238,56 +211,6 @@ def get_userID(o: parse.SplitResult):
         with open(Path(__file__).parent / "config.ini", "w", encoding="utf-8") as configfile:
             config.write(configfile)
         exit()
-
-
-def progress_bar(speed_dict):
-    slow_flag = 0
-    message = "" * 50
-    while True:
-        if speed_dict["total_length"] < 0 or speed_dict["current_length"] < 0:
-            time.sleep(1)
-        else:
-            speed = (sum(speed_dict["size"]) / (time.time() - min(speed_dict["time"]))) if max(speed_dict["time"]) > min(speed_dict["time"]) else 0
-            speed_dict["max_speed"] = max(speed_dict["max_speed"], speed)
-            if speed <= speed_dict["max_speed"] / 2 and speed < 1024 * 1024:  # 包括开始时速度为0的情况, 速度<1M/s才考虑重置
-                slow_flag += 1
-            else:
-                slow_flag = 0
-            if slow_flag >= 30:  # 超过30秒速度<200kB/s
-                speed_dict["stop"] = True
-                break
-            done = int(50 * speed_dict["current_length"] / speed_dict["total_length"])  # https://stackoverflow.com/a/21868231/5340217
-            est = int((speed_dict["total_length"] - speed_dict["current_length"]) / speed) if speed else -1
-            print(f'\r{" " * (sum(2 if "\u4e00" <= char <= "\u9fff" else 1 for char in message))}', end="")
-            message = f'[{"=" * done}{" " * (50-done)}] {sizeof_fmt(speed)}/s {sizeof_fmt(speed_dict["current_length"])}/{sizeof_fmt(speed_dict["total_length"])} 剩余: {str(datetime.timedelta(seconds=est)) if est>=0 else "未知"}'
-            print(f"\r{message}", end="")
-            time.sleep(1)
-
-
-def progress_bar_2(speed_dict):
-    from alive_progress import alive_bar
-
-    slow_flag = 0
-    with alive_bar(total=speed_dict["total_length"], manual=True, monitor="{count}/{total} [{percent:.0%}]") as bar:
-        while True:
-            if speed_dict["total_length"] < 0 or speed_dict["current_length"] < 0:
-                bar(0)
-                time.sleep(1)
-            else:
-                speed = (sum(speed_dict["size"]) / (time.time() - min(speed_dict["time"]))) if max(speed_dict["time"]) > min(speed_dict["time"]) else 0
-                speed_dict["max_speed"] = max(speed_dict["max_speed"], speed)
-                if speed <= speed_dict["max_speed"] / 2 and speed < 1024 * 1024:  # 包括开始时速度为0的情况, 速度<1M/s才考虑重置
-                    slow_flag += 1
-                else:
-                    slow_flag = 0
-                if slow_flag >= 30:  # 超过30秒速度<200kB/s
-                    speed_dict["stop"] = True
-                    break
-                done = int(50 * speed_dict["current_length"] / speed_dict["total_length"])  # https://stackoverflow.com/a/21868231/5340217
-                est = int((speed_dict["total_length"] - speed_dict["current_length"]) / speed) if speed else -1
-                # print(f'\r[{"=" * done}{" " * (50-done)}] {sizeof_fmt(speed)}/s {sizeof_fmt(speed_dict["current_length"])}/{sizeof_fmt(speed_dict["total_length"])} 预计: {str(datetime.timedelta(seconds=est))}', end='')
-                time.sleep(1)
-                bar(speed_dict["current_length"] / speed_dict["total_length"])
 
 
 def monitor():
